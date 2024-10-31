dd53ed516f12b5535b10ee80ff960fa0359a7621
Rafinizamudin <rafi.nizamudin01@gmail.com>
2024-10-31 20:00:43 +0700

    UPDATE

---
+++ Pertemuan 5/latihan10.js
@@ -0,0 +1,7 @@
+document.getElementById("lomw").innerHTML = "Ini inner";
+document.write("Ini Writer");
+window.alert("Ini Altert");
+console.info("Ini Info");
+console.warn("Ini Warn");
+console.error("Ini Error");
+console.table(["audi", "inzaqi", "Panca"]);
